e05bfbdac6aea831c509e7fbb10bbf262c1ef5ad
Dempoleon <dempoleon@gmail.com>
2022-12-05 20:09:24 -0800

    implemented contact form

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,14 +1,78 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 
 export default function Contact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleName(event) {
+        setName(event.target.value)
+    }
+
+    function handleEmail(event) {
+        setEmail(event.target.value)
+    }
+
+    function handleMessage(event) {
+        setMessage(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        // event.preventDefault();
+        if(!name) {
+            alert('name is requried')
+            return
+        }
+        if(!email) {
+            alert('email is requried')
+            return
+        }
+        if(!message) {
+            alert('message is requried')
+            return
+        }
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
     return (
-        <div className="page">
-            <p>
-                this is the contact page
-            </p>
-            <input>
-            </input>
+        <div className="page contact">
+            
+            <div className='contact-form'>
+                <input
+                    name="name"
+                    value={name}
+                    type="text"
+                    placeholder="name"
+                    onChange={handleName}
+                >
+                </input>
+                <input
+                    name="email"
+                    value={email}
+                    type="text"
+                    placeholder="email"
+                    onChange={handleEmail}
+                >
+                </input>
+                <input
+                    name="message"
+                    value={message}
+                    type="text"
+                    placeholder="message"
+                    onChange={handleMessage}
+                >
+                </input>
+                <button
+                    type='submit'
+                    onClick={handleSubmit}
+
+                >
+                    Submit
+                </button>
+            </div>
         </div>
     )
 }
