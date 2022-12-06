4611e2f101232fb372c9d6005665dc39d2656da7
Dempoleon <dempoleon@gmail.com>
2022-12-05 20:14:12 -0800

    working footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -18,9 +18,9 @@ export default function Footer() {
 
     return (
         <div className='stickyFooter'>
-            <a>GitHub</a>
-            <a>LinkedIn</a>
-            <a>Medium</a>
+            <a href='https://github.com/Dempleon'>GitHub</a>
+            <a href='https://www.linkedin.com/in/daryl-empleo/'>LinkedIn</a>
+            <a href='https://medium.com/@dempoleon'>Medium</a>
         </div>
     )
 }
